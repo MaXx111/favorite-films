69232a95fafd053b8438d754c3a41747b62b5b24
Sidorov Maksim <maxx11167@gmail.com>
2023-06-30 17:01:08 +0300

    fix

--- src/App.tsx
+++ src/App.tsx
@@ -9,9 +9,9 @@ function App() {
   return (
     <>
       <Routes>
-        <Route path="/favorite-films/" element={<MainFilms />} />
-        <Route path="/favorite-films/favorites" element={<FavoritesFilms />} />
-        <Route path="/favorite-films/:id" element={<FilmPage />} />
+        <Route path="favorite-films/" element={<MainFilms />} />
+        <Route path="favorite-films/favorites" element={<FavoritesFilms />} />
+        <Route path="favorite-films/:id" element={<FilmPage />} />
       </Routes>
     </>
   )
